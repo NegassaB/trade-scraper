843a6685ab2f3bb61f171536a0be23531afc53bf
Negassa Berhanu <negassab16@gmail.com>
2022-01-26 17:44:48 +0300

    update trade_watcher.csv to trade_watcher.* cause anything named trade_watcher should be ignored, update sandbox to use the built-in WebDriverWait feature of selenium instead of time.sleep(), update the code accordingly, remove completed todos, unneeded imports in trade_scraper. Also update the code in accordance with the new setup

--- sandbox.py
+++ sandbox.py
@@ -1,35 +1,60 @@
-import csv
 import time
 
 from bs4 import BeautifulSoup as bs
-from bs4.element import NavigableString, Tag
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 browser_options = Options()
-browser_options.add_argument("--headless")
+# browser_options.add_argument("--headless")
 browser_options.add_argument("--incognito")
 
 browser = webdriver.Chrome(options=browser_options)
 browser.get("https://v3.aggr.trade/")
-time.sleep(30)
+# time.sleep(60)
+# browser.refresh()
 
-soup = bs(browser.page_source, "html.parser")
-result = soup.find_all('div', class_='trades-list hide-scrollbar -logos')
 
-trade_list = {}
-for res in result:
-    for list_items in res:
-        trade_list[list_items.attrs['title']] = {'class': list_items.attrs['class']}
-        divs = list_items.find_all('div')
-        while len(divs) > 0:
-            div = divs.pop(0)
+def get_soup():
+    try:
+        # elem_present = WebDriverWait(browser, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'trade')))
+        elem_present = WebDriverWait(browser, 10).until(EC.visibility_of_any_elements_located((By.CLASS_NAME, 'trade')))
+        # elem_present = WebDriverWait(browser, 10).until(EC.text_to_be_present_in_element((By.TAG_NAME, 'title'), 'BTCUSD '))
+        # elem_present = WebDriverWait(browser, 10, 2).until(EC.presence_of_all_elements_located(By.TAG_NAME, 'li'))
+    except Exception:
+        print("page is not ready, retrying")
+        time.sleep(1)
+        get_soup()
+    else:
+        if elem_present:
+            print("page is ready to be scrapped")
+            return bs(browser.page_source, "html.parser")
+        else:
+            print("WHAT THE ACTUAL FUCK")
+
+
+soup = get_soup()
+if soup:
+    result = soup.find_all('div', class_='trades-list hide-scrollbar -logos')
+
+trade_dict = {}
+for outter_div in result:
+    print(type(outter_div))
+    for li in outter_div:
+        print(type(li))
+        trade_dict[li.attrs['title']] = {'class': li.attrs['class']}
+        inner_divs = li.find_all('div')
+        while len(inner_divs) > 0:
+            div = inner_divs.pop(0)
+            print(type(div))
             if 'trade__price' in div.attrs['class']:
-                trade_list[list_items.attrs['title']].update({'trade_price': div.text.strip()})
+                trade_dict[li.attrs['title']].update({'trade_price': div.text.strip()})
             if 'data-timestamp' in div.attrs:
-                trade_list[list_items.attrs['title']].update({'trade_timestamp': div.attrs['data-timestamp']})
+                trade_dict[li.attrs['title']].update({'trade_timestamp': div.attrs['data-timestamp']})
             if 'trade__amount' in div.attrs['class']:
                 spans = div.find_all('span')
                 while len(spans) > 0:
@@ -38,53 +63,11 @@ for res in result:
                         pass
                     else:
                         if 'trade__amount__quote' in span.attrs['class']:
-                            trade_list[list_items.attrs['title']].update({'trade_amount_quote': span.text.strip()})
+                            trade_dict[li.attrs['title']].update({'trade_amount_quote': span.text.strip()})
                         if 'trade__amount__base' in span.attrs['class']:
-                            trade_list[list_items.attrs['title']].update({'trade_amount_base': span.text.strip()})
+                            trade_dict[li.attrs['title']].update({'trade_amount_base': span.text.strip()})
+    #     break
+    # break
 
-print(trade_list)
 
-{
-    'BINANCE_FUTURES:btcusdt': {
-        'class': ['trade', '-BINANCE_FUTURES', '-buy', '-level-0'],
-        'trade_price': '36557',
-        'trade_amount_quote': '167K',
-        'trade_amount_base': '4.574',
-        'trade_timestamp': '1643115788860'
-    },
-    'BYBIT:BTCUSD': {
-        'class': ['trade', '-BYBIT', '-sell', '-level-0'],
-        'trade_price': '36558.5',
-        'trade_amount_quote': '120K',
-        'trade_amount_base': '3.2851',
-        'trade_timestamp': '1643115800000'
-    },
-    'BINANCE_FUTURES:btcusd_perp': {
-        'class': ['trade', '-BINANCE_FUTURES', '-sell', '-level-0'],
-        'trade_price': '36566.8',
-        'trade_amount_quote': '145K',
-        'trade_amount_base': '3.9544',
-        'trade_timestamp': '1643115795237'
-    },
-    'BITMEX:XBTUSD': {
-        'class': ['trade', '-BITMEX', '-sell', '-level-0'],
-        'trade_price': '36551.0',
-        'trade_amount_quote': '140K',
-        'trade_amount_base': '3.8304',
-        'trade_timestamp': '1643115815532'
-    },
-    'FTX:BTC-PERP': {
-        'class': ['trade', '-FTX', '-sell', '-level-0'],
-        'trade_price': '36599.0',
-        'trade_amount_quote': '115K',
-        'trade_amount_base': '3.1444',
-        'trade_timestamp': '1643115795394'
-    },
-    'BYBIT:BTCUSDT': {
-        'class': ['trade', '-BYBIT', '-sell', '-level-0'],
-        'trade_price': '36542.0',
-        'trade_amount_quote': '180K',
-        'trade_amount_base': '4.939',
-        'trade_timestamp': '1643115800000'
-    }
-}
+print(trade_dict)
--- trade_scraper.py
+++ trade_scraper.py
@@ -1,118 +1,64 @@
-import csv
+import json
 import time
 
 from bs4 import BeautifulSoup as bs
-from bs4.element import NavigableString, Tag
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
 browser_options = Options()
-# browser_options.add_argument("--headless")
+browser_options.add_argument("--headless")
 browser_options.add_argument("--incognito")
 
 browser = webdriver.Chrome(options=browser_options)
 browser.get("https://v3.aggr.trade/")
 time.sleep(30)
 
-# 49800 -- trade__price
-# 509K -- trade__amount__quote
-# 29.8 -- trade__amount__base
-# Sell -- li inside class attr
-# BYBIT -- li inside class attr
-# todo: write to json
-# todo: no duplicates
-
-
-soup = bs(browser.page_source, "html.parser")
-result = soup.find_all('div', class_='trades-list hide-scrollbar -logos')
-
-trade_list = {}
-for res in result:
-    for list_items in res:
-        print(f"{list_items.attrs['class']} -- {list_items.attrs['title']}") 
-        divs = list_items.find_all('div')
-        while len(divs) > 0:
-            div = divs.pop(0)
-            if 'trade__price' in div.attrs['class']:
-                trade_list[f"{list_items.attrs['class']} -- {list_items.attrs['title']}"] = {'trade__price': div.text}
-            elif 'data-timestamp' in div.attrs:
-                trade_list[f"{list_items.attrs['class']} -- {list_items.attrs['title']}"] = {'trade__timestamp': div.text}
-            elif 'trade__amount' in div.attrs['class']:
-                spans = div.find_all('span')
-                print(spans)
-                while len(spans) > 0:
-                    span = spans.pop(0)
-                    if isinstance(span, NavigableString):
-                        pass
-                    else:
-                        if 'trade__amount__quote' in span.attrs['class']:
-                            trade_list[f"{list_items.attrs['class']} -- {list_items.attrs['title']}"] = {'trade__amount__quote': span.text}
-                        elif 'trade__amount__base' in span.attrs['class']:
-                            trade_list[f"{list_items.attrs['class']} -- {list_items.attrs['title']}"] = {'trade__amount__base': span.text}
-                        else:
-                            pass
-            else:
-                pass
-        break
-    break
-
-
-for res in result:
-    for list_items in res:
-        print(f"{list_items.attrs['class']} -- {list_items.attrs['title']}")
-        for content in list_items:
-            if isinstance(content, NavigableString):
-                pass
-            else:
-                print(f"{content.attrs}\n")
-                # print(f"{content.contents}")
-                # print(f"{content.children}\n")
-                if "trade__amount" in content.attrs:
-                    print(content.span.next_sibling)
-                for child in content:
-                    if isinstance(child, Tag):
-                        for sib in child:
-                            # type(sib)
-                            if isinstance(sib, NavigableString):
-                                pass
-                            else:
-                                print(f"{sib.attrs} {sib.text.strip()}")
-                            # print(sib.attrs)
-                    print(f"{content.attrs['class']} {content.text.strip()}")
-                # if 'data-timestamp' in content.attrs:
-                #     print(content.attrs['data-timestamp'])
-            # print(content.class)
-        break
-    break
-
 
 def get_soup():
     return bs(browser.page_source, "html.parser")
 
 
-def extract_save_csv():
+def save_2_file(trade_dict):
+    with open('trade_watcher.json', 'a') as trade_watcher:
+        json.dump(trade_dict, trade_watcher)
+
+    print(trade_dict)
+
+
+def extract_trades():
+    trade_dict = {}
     soup = get_soup()
     result = soup.find_all('div', class_='trades-list hide-scrollbar -logos')
-    with open('trade_watcher.csv', 'a', newline="") as trade_watcher:
-        writeCSV = csv.writer(trade_watcher)
-        for res in result:
-            for list_item in res:
-                writeCSV.writerow([f"{list_item.attrs['class'][1]}"])
-                for content in list_item:
-                    writeCSV.writerow([f"\t\t{content.attrs['class']}"])
-                    if '-timestamp' in content.attrs['class']:
-                        for val in content:
-                            writeCSV.writerow([f"\t\t{val.text}"])
-                    else:
-                        writeCSV.writerow([f"\t\t{content.text}\n"])
-        writeCSV.writerow(["\n"])
+    for res in result:
+        for list_items in res:
+            trade_dict[list_items.attrs['title']] = {'class': list_items.attrs['class']}
+            divs = list_items.find_all('div')
+            while len(divs) > 0:
+                div = divs.pop(0)
+                if 'trade__price' in div.attrs['class']:
+                    trade_dict[list_items.attrs['title']].update({'trade_price': div.text.strip()})
+                if 'data-timestamp' in div.attrs:
+                    trade_dict[list_items.attrs['title']].update({'trade_timestamp': div.attrs['data-timestamp']})
+                if 'trade__amount' in div.attrs['class']:
+                    spans = div.find_all('span')
+                    while len(spans) > 0:
+                        span = spans.pop(0)
+                        if 'class' not in span.attrs.keys():
+                            pass
+                        else:
+                            if 'trade__amount__quote' in span.attrs['class']:
+                                trade_dict[list_items.attrs['title']].update({'trade_amount_quote': span.text.strip()})
+                            if 'trade__amount__base' in span.attrs['class']:
+                                trade_dict[list_items.attrs['title']].update({'trade_amount_base': span.text.strip()})
+    save_2_file(trade_dict)
 
 
 if __name__ == "__main__":
     while True:
-        extract_save_csv()
+        extract_trades()
         print("sleeping")
+        browser.refresh()
         time.sleep(2)
         print("DONE sleeping")
